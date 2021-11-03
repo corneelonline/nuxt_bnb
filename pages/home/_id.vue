f21a91962dd6bf67eeaf2e64040d91738685936a
Marco Verheul <46519750+corneelonline@users.noreply.github.com>
2021-11-03 14:32:56 +0100

    Add schema.org to the product page

--- pages/home/_id.vue
+++ pages/home/_id.vue
@@ -6,11 +6,34 @@
     <PropertyMap :home="home" />
     <PropertyReviews :reviews="reviews" />
     <PropertyHost :user="user" />
+    <script type="application/ld+json" v-html="getSchema"></script>
   </div>
 </template>
 
 <script>
 export default {
+  computed: {
+    getSchema(){
+      return JSON.stringify({
+        "@context": "http://schema.org",
+        "@type": "BedAndBreakfast",
+        "name": this.home.title,
+        "image": $img(this.home.images[0], {width: 1200}, {provider: 'cloudinary'}),
+        "address":{
+          "@type": "PostalAddress",
+          "addressLocation": this.home.location.city,
+          "addressRegion": this.home.location.state,
+          "postalCode": this.home.location.zipcode,
+          "streetAddress": this.home.location.address,
+        },
+        "aggregateRating":{
+          "@type": "AggregateRating",
+          "ratingValue": this.home.reviewValue,
+          "reviewCount": this.home.reviewCount,
+        }
+      })
+    }
+  },
   head() {
     return {
       title: this.home.title,
